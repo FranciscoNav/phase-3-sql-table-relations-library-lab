5c395e9fb2bfe9419a7b5e52ea2ea5ed387261b2
Francisco <fnaveira11@gmail.com>
2021-05-28 15:22:43 -0400

    commit tree 122d0fe5cc5879a32502e07fe6c3ef5daf9860e9

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,16 +12,15 @@ end
 
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors INNER JOIN series ON author_id = authors.id"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 
-
 def select_series_title_with_most_human_characters
-  "SELECT title, characters.species FROM series INNER JOIN characters ON characters.author_id = series.author_id WHERE count(characters.species) > 3"
+  "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON  character_books.book_id = books.id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT character.name, COUNT(character_books.book_id)FROM characters INNER JOIN character_books ON character_id = characters.id GROUP BY characters.name ASC"
+  "SELECT characters.name, COUNT(*) AS num_characters FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY num_characters DESC, characters.name ASC" 
 end
